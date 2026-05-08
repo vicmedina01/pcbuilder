814eef6ba105be693fd6fae71001c725d9074096
Victor <victorfabian115@gmail.com>
2026-05-08 10:19:12 -0600

    feat: footer added to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar.jsx";
+import  Footer from "@/components/footer.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white px-6 py-4 text-center">
+      &copy; 2026 PCBuilder. All rights reserved.
+    </footer>
+  )
+}
