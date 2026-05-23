5e13e529a973014e678ce66685049502cc5d7edb
Victor <victorfabian115@gmail.com>
2026-05-23 10:12:25 -0600

    feat: prisma setup with supabase database

---
+++ prisma.config.ts
@@ -0,0 +1,17 @@
+import path from "path"
+import { defineConfig } from "prisma/config"
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: path.join("prisma", "schema.prisma"),
+  migrate: {
+    async adapter() {
+      const { PrismaPg } = await import("@prisma/adapter-pg")
+      const { default: pg } = await import("pg")
+      const pool = new pg.Pool({
+        connectionString: process.env.DIRECT_URL,
+      })
+      return new PrismaPg(pool)
+    },
+  },
+})
